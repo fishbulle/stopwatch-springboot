7d68ca805d4eaae3131d07b3f13918d0883eee00
Angelina <linaemalmros@gmail.com>
2023-04-11 09:04:05 +0200

    created repository interface

--- src/main/java/com/example/tidtagarur/TimerRepository.java
+++ src/main/java/com/example/tidtagarur/TimerRepository.java
@@ -1,4 +1,10 @@
 package com.example.tidtagarur;
 
-public class TimerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TimerRepository extends JpaRepository<TimerEntity, UUID> {
 }
