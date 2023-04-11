a208f13098be1e4fda14bc32c74db17d38d36466
Angelina <linaemalmros@gmail.com>
2023-04-11 09:08:55 +0200

    anoterat service och controller

--- src/main/java/com/example/tidtagarur/TimerController.java
+++ src/main/java/com/example/tidtagarur/TimerController.java
@@ -1,4 +1,7 @@
 package com.example.tidtagarur;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class TimerController {
 }
--- src/main/java/com/example/tidtagarur/TimerService.java
+++ src/main/java/com/example/tidtagarur/TimerService.java
@@ -1,4 +1,7 @@
 package com.example.tidtagarur;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TimerService {
 }
